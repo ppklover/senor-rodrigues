f401180fa1c4d0b6b7556db523999ec62a5108ea
ppklover <rodolfo.one@icloud.com>
2025-06-09 21:09:36 -0300

    Atualiza app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Barbeiros from "./pages/Barbeiros";
-import Agendamentos from "./pages/Agendamentos";
 import Produtos from "./pages/Produtos";
 import Estoque from "./pages/Estoque";
-import Relatorios from "./pages/Relatorios";
-import Servicos from "./pages/Servicos"; // <- NOVO IMPORT
+import Servicos from "./pages/Servicos";
+import Clientes from "./pages/Clientes"; // NOVO
 
 export default function App() {
   return (
@@ -14,12 +14,11 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-            <Route path="/barbeiros" element={<Barbeiros />} />
-        <Route path="/agendamentos" element={<Agendamentos />} />
+        <Route path="/barbeiros" element={<Barbeiros />} />
         <Route path="/produtos" element={<Produtos />} />
         <Route path="/estoque" element={<Estoque />} />
-        <Route path="/relatorios" element={<Relatorios />} />
-        <Route path="/servicos" element={<Servicos />} /> {/* NOVA ROTA */}
+        <Route path="/servicos" element={<Servicos />} />
+        <Route path="/clientes" element={<Clientes />} />
       </Routes>
     </Router>
   );
